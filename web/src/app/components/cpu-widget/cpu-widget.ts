daf0b827806d9af94929a02d150824f861f3efd4
Joshua Mellon <66973427+JoshuaMellon@users.noreply.github.com>
2026-08-20 11:56:38 +0100

    cpu widget

--- web/src/app/components/cpu-widget/cpu-widget.ts
+++ web/src/app/components/cpu-widget/cpu-widget.ts
@@ -2,9 +2,11 @@ import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SystemStore } from "../../services/systemStore.service";
 
+import { MatCardModule } from "@angular/material/card";
+
 @Component({
     selector: "app-cpu-widget",
-    imports: [CommonModule],
+    imports: [CommonModule, MatCardModule],
     templateUrl: "./cpu-widget.html",
     styleUrls: ["./cpu-widget.scss"],
 })
