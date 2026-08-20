eee1ddabd453b8e5fbb2c8761e4b1560ae801d08
Joshua Mellon <66973427+JoshuaMellon@users.noreply.github.com>
2026-08-20 17:58:44 +0100

    cpu + machine looking better

--- web/src/app/components/cpu-widget/cpu-widget.ts
+++ web/src/app/components/cpu-widget/cpu-widget.ts
@@ -6,10 +6,11 @@ import { MatCardModule } from "@angular/material/card";
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { MatIconModule } from "@angular/material/icon";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @Component({
     selector: "app-cpu-widget",
-    imports: [CommonModule, MatCardModule, MatTabsModule, MatProgressBarModule, MatIconModule],
+    imports: [CommonModule, MatCardModule, MatTabsModule, MatProgressBarModule, MatIconModule, MatProgressSpinnerModule],
     templateUrl: "./cpu-widget.html",
     styleUrls: ["./cpu-widget.scss"],
 })
--- web/src/app/components/machine-widget/machine-widget.ts
+++ web/src/app/components/machine-widget/machine-widget.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from "@angular/core";
-
+import { formatUptime } from "../../utils/times.utils";
 import { MatCardModule } from "@angular/material/card";
 import { SystemStore } from "../../services/systemStore.service";
 import { MatIconModule } from "@angular/material/icon";
@@ -14,4 +14,42 @@ import { DatePipe } from "@angular/common";
 })
 export class MachineWidget {
     readonly system = inject(SystemStore);
+
+    readonly formatUptime = formatUptime;
+
+    returnUptime(uptime: number): string {
+        const times = formatUptime(uptime);
+
+        let uptimeString = "";
+
+        if (times.years > 0) {
+            uptimeString += `${times.years}y `;
+        }
+
+        if (times.months > 0) {
+            uptimeString += `${times.months}mo `;
+        }
+
+        if (times.weeks > 0) {
+            uptimeString += `${times.weeks}w `;
+        }
+
+        if (times.days > 0) {
+            uptimeString += `${times.days}d `;
+        }
+
+        if (times.hours > 0) {
+            uptimeString += `${times.hours}h `;
+        }
+
+        if (times.minutes > 0) {
+            uptimeString += `${times.minutes}m `;
+        }
+
+        if (times.seconds > 0) {
+            uptimeString += `${times.seconds}s`;
+        }
+
+        return uptimeString.trim();
+    }
 }
